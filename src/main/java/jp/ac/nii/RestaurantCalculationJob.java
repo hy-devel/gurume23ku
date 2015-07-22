3e85b6cf5edfeafcab5f7bcbd460cfa7f717111c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2015-07-22 21:28:38 +0900

    Unimport org.apache.hadoop.io.IntWritable as unused.

--- src/main/java/jp/ac/nii/RestaurantCalculationJob.java
+++ src/main/java/jp/ac/nii/RestaurantCalculationJob.java
@@ -3,7 +3,6 @@ package jp.ac.nii;
 import java.io.IOException;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
